818672b585a9de36a601832ee5b3074cb3db0a82
James Kim <eodnjs2998@gmail.com>
2015-04-01 23:22:56 +0900

    누락.

--- Assets/Editor/NeoTestMenu.cs
+++ Assets/Editor/NeoTestMenu.cs
@@ -2,11 +2,11 @@
 
 namespace HX
 {
-	public static class NeoTestMenu
+	public static class TestMenu
 	{
 		private const string NEO = "Assets/GO/Neo/Test/test_neo.unity";
 
-		[MenuItem("Test/Neo/NeoTest")]
+		[MenuItem("HX/Test/Neo/NeoTest")]
 		private static void NeoTest()
 		{
 			EditorSceneHelper.Transfer(NEO);
---
+++ Assets/GO/Cell/Editor/CellGridTestEditor.cs
@@ -0,0 +1,21 @@
+﻿using Gem;
+using UnityEditor;
+using UnityEngine;
+
+namespace HX
+{
+	[CustomEditor(typeof(CellGridTest))]
+	public class CellGridTestEditor : Editor<CellGridTest>
+	{
+		public override void OnInspectorGUI()
+		{
+			base.OnInspectorGUI();
+
+			if (GUILayout.Button("reload"))
+			{
+				target.grid.DestroyAll();
+				target.Load();
+			}
+		}
+	}
+}

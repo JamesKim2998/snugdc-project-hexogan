1f215d201e1e26a4218c2826799609f9ebe6ff79
James Kim <eodnjs2998@gmail.com>
2015-04-14 21:37:57 +0900

    LobbyTest added.

---
+++ Assets/UI/Scene/LobbyTest.cs
@@ -0,0 +1,19 @@
+﻿#if UNITY_EDITOR
+
+using UnityEngine;
+
+namespace HX.UI
+{
+	public class LobbyTest : MonoBehaviour
+	{
+		void Start()
+		{
+			if (!DisketManager.isLoaded)
+				DisketManager.LoadOrDefault("test");
+
+			UserManager.neoStructure = DisketManager.saveData.neoStructure;
+		}
+	}
+}
+
+#endif

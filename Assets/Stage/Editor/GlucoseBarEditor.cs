90ae1fc477aa86df6fc009356a709b5087d74b1f
James Kim <eodnjs2998@gmail.com>
2015-05-05 00:26:23 +0900

    GlucoseBar view added.

---
+++ Assets/Stage/Editor/GlucoseBarEditor.cs
@@ -0,0 +1,33 @@
+﻿using Gem;
+using UnityEditor;
+using UnityEngine;
+
+namespace HX
+{
+	[CustomEditor(typeof(GlucoseBar))]
+	public class GlucoseBarEditor : Editor<GlucoseBar>
+	{
+		private int mMax;
+		private float mValue;
+
+		protected override void OnEnable()
+		{
+			mMax = target.max;
+			mValue = target.value;
+		}
+
+		public override void OnInspectorGUI()
+		{
+			base.OnInspectorGUI();
+
+			mMax = EditorGUILayout.IntField("max", mMax);
+			mValue = EditorGUILayout.FloatField("value", mValue);
+
+			if (GUILayout.Button("commit"))
+			{
+				target.max  = mMax;
+				target.value = mValue;
+			}
+		}
+	}
+}
---
+++ Assets/Stage/UI/GlucoseBar/GlucoseBar.cs
@@ -0,0 +1,47 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+
+namespace HX
+{
+	public class GlucoseBar : MonoBehaviour
+	{
+		private const float THRESHHOLD = 0.1f;
+		private const float LENGTH_MAX = 1000;
+		private const float VALUE_TO_LEN = 1;
+
+		private float mValue;
+		public float value
+		{
+			get { return mValue; }
+			set
+			{
+				if (Mathf.Abs(this.value - value) < THRESHHOLD)
+					return;
+
+				mValue = value;
+				Refresh();
+			}
+		}
+
+		private int mMax;
+		public int max
+		{
+			get { return mMax; }
+			set
+			{
+				mMax = value;
+				mBar.width = (int)Mathf.Min(value * VALUE_TO_LEN, LENGTH_MAX);
+				Refresh();
+			}
+		}
+
+		[SerializeField, UsedImplicitly] private UISprite mBar;
+		[SerializeField, UsedImplicitly] private UIProgressBar mProgress;
+
+		void Refresh()
+		{
+			if (max != 0)
+				mProgress.value = value / max;
+		}
+	}
+}
